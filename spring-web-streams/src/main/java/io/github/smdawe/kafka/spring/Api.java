02530e3eb15f5e0f964c22dc8485adea5e567ec4
Simon Dawe <simon.dawe@oodlefinance.com>
2022-05-10 16:46:34 +0100

    use join

--- spring-web-streams/src/main/java/io/github/smdawe/kafka/spring/Api.java
+++ spring-web-streams/src/main/java/io/github/smdawe/kafka/spring/Api.java
@@ -8,11 +8,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 public class Api {
@@ -30,9 +32,11 @@ public class Api {
   @PostMapping(value = MESSAGE_ENDPOINT, produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<Void> message(@RequestBody MessageBody messageBody) {
     try {
-      kafkaTemplate.send(topic, UUID.randomUUID().toString(), messageBody.getValue());
+      kafkaTemplate.send(topic, UUID.randomUUID().toString(), messageBody.getValue()).completable().join();
+
       return new ResponseEntity<>(HttpStatus.CREATED);
     } catch (Throwable t) {
+      LOG.error(t.getMessage(), t);
       return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
